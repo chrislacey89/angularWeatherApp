b3f510a630d794c3c0c0e1b341073fb0de057e17
Christopher Lacey <christopherlacey@Chris-Mac.local>
2020-04-18 10:03:13 -0400

    added weather data fetching

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
+// const url = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${key}`;
+//api.openweathermap.org/data/2.5/forecast?q=london&appid=8d666dc480691f6b1ddc912e88cf0ca5
+// const key = '8d666dc480691f6b1ddc912e88cf0ca5';
+// let city = '';
+
 // MODULE
-var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
+http: var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
 
 // ROUTES
 weatherApp.config(function ($routeProvider) {
@@ -28,16 +33,37 @@ weatherApp.controller('homeController', [
   function ($scope, cityService) {
     $scope.city = cityService.city;
     $scope.$watch('city', function () {
-      nameService.city = $scope.city;
-      console.log(nameService.city);
+      cityService.city = $scope.city;
+      console.log(cityService.city);
     });
   },
 ]);
 
 weatherApp.controller('forecastController', [
   '$scope',
+  '$resource',
   'cityService',
-  function ($scope, cityService) {
-    $scope.city = cityService;
+  function ($scope, $resource, cityService) {
+    $scope.city = cityService.city;
+
+    $scope.weatherAPI = $resource(
+      'http://api.openweathermap.org/data/2.5/forecast/',
+      { callback: 'JSON_CALLBACK' },
+      { get: { method: 'JSONP' } }
+    );
+
+    $scope.weatherResult = $scope.weatherAPI.get({
+      q: $scope.city,
+      appid: '8d666dc480691f6b1ddc912e88cf0ca5',
+    });
+    console.log($scope.weatherResult);
+
+    $scope.convertToFahrenheit = function (degK) {
+      return Math.round(1.8 * (degK - 273) + 32);
+    };
+
+    $scope.convertToDate = function (dt) {
+      return new Date(dt * 1000);
+    };
   },
 ]);
